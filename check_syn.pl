b83120f006104bf44ca3b0c9011588c76bc006e8
larrymooney <48460794+larrymooney@users.noreply.github.com>
2019-03-16 16:07:46 -0500

    Updated to include file options, simplify code

--- check_syn.pl
+++ check_syn.pl
@@ -1,23 +1,15 @@
 =comment
 
-Usage:
-#adds homographs where necessary and removes \hm Default Value (set to 100) to unique entries.
-#hm Default Value is placed in an SFM when using this script with the -u option. Creates log file add_hm.<time>.log
-#
-	perl check_syn.pl FILENAME.SFM  
-
-
-#adds homographs where necessary and adds \hm Default Value (set to 100) to unique entries.  
-#Creates log file add_hm_u.<time>.log
-#
-	perl check_syn.pl FILENAME.SFM -u
-
-
-#input file is an SFM file.  This file will be opl'd, processed and de_opl'd. 
-#output file is an SFM file.
-#
+Rules for matching synonym or antonym referents:
+1. Is the it found as a headword? (If so, Ref Fnd = "Y")
+2. Is the it reciprocated? Does a similar marker exist under the headword for the original headword? (If so, Reciprocal = Headword.Homograph)
+3. Is the it ambiguous? 
+   a. Are there multiple headword referents with no reciprocals? (If so, Ambiguous = "Y")
+   b. Are there multiple headword referents with multiple reciprocals? (If so, Ambiguous = "Y")   
 
 #########  ASSUMPTIONS  ###############
+
+Homographs and sense numbers are present and explicit. 
 
 =cut
 #
@@ -29,77 +21,115 @@ use warnings;
 use Data::Dumper qw(Dumper);
 use Time::Piece;
 use oplStuff;
+use Config::Tiny;
+use Path::Tiny qw(path);
 #
-my $date = Time::Piece->new;
+# Read and process .ini file
+#
+our $config = Config::Tiny->read( 'ChkSyn.ini' );
+#
+die "Couldn't find the INI file\nQuitting" if !$config;
+# Language
+our $language = $config->{ChkSyn}->{lang};
+# File path
+our $fpath = $config->{ChkSyn}->{fpath};
+# Log file 
+our $flog = $config->{ChkSyn}->{flog};
+# SFM input file
+our $ifsfm = $config->{ChkSyn}->{ifsfm};
+# Report output file 
+our $ofrpt = $config->{ChkSyn}->{ofrpt};
+# Lexeme marker
+our $lexmarker = $config->{ChkSyn}->{lexmarker};
+# Homograph marker
+our $hommarker = $config->{ChkSyn}->{hommarker};
+# Synonym marker
+our $synmarker = $config->{ChkSyn}->{synmarker};
+# Sense marker
+our $snsmarker = $config->{ChkSyn}->{snsmarker};
+#
+#
+our $date = Time::Piece->new;
 $date->time_separator("");
 $date->date_separator("");
-my $tm = $date->datetime;
+our $tm = $date->datetime;
 #
-my $infile ="";
-my $scriptname = $0;
-my $logfile = "$tm.log";
-my %file_hash;
-my %scalar_count;
-my @dups;
-my @tmpRec;
-my $DUPLICATE = 0;
-my $ADD_HM_TO_UNIQUE = 0;
-my $DEFAULT_HM = 100;
-my $numargs = $#ARGV + 1;
-
-
-if ( $numargs == 0 ){
-	 die "Usage: perl check_syn.pl FILENAME [-u]"; 
-}
-elsif ( $numargs == 1 ){
-	$infile = $ARGV[0]; 
-}
-else {
-	$infile = $ARGV[0]; 
-	if ($ARGV[1] eq "-u"){ $ADD_HM_TO_UNIQUE = 1; }
-	else { die "Usage: perl check_syn.pl FILENAME [-u]"; }
-}
-
-
-#open(my $fhlogfile, '>:encoding(UTF-8)', $logfile) 
-#	or die "Could not open file '$logfile' $!";
-
- 
-#write_to_log("Input file $infile");
-
-#opl the file - i.e. put each record on a line.
-my @opld_file = opl_file($infile);
-
-#Create a table of lexemes and syn in the following format
+our $scriptname = $0;
+our %file_hash;
+our %scalar_count;
+our @dups;
+our @tmpRec;
+our $DUPLICATE = 0;
+our $ADD_HM_TO_UNIQUE = 0;
+our $DEFAULT_HM = 100;
+our $numargs = @ARGV;
 #
-#[lexeme][sn][syn][slex_found][syn_recip]
+# USE <STDIN> entered file names if entered, otherwise use .ini file values
 #
+if ( $numargs > 0 )
+{
+	$fpath = "";
+	$ifsfm = $ARGV[0]; 
+	if ( $numargs > 1 )
+	{
+		if ($ARGV[1] eq "-u")
+		{ 
+			$ADD_HM_TO_UNIQUE = 1; 
+			if ( $numargs > 2 )
+			{
+				$ofrpt = $ARGV[2] 		
+			}
+		}
+		else 
+		{ 
+			$ofrpt = $ARGV[1] 
+		}
+	}
+}	
 #
-#create hash of the entire file using lexeme[hm] as key
+open(our $fhlogfile, '>:encoding(UTF-8)', $fpath.$flog) 
+	or die "Could not open file '$flog' $!";
 #
-foreach my $line (@opld_file) {
-my $hm;
-my $key;
-my $sn = 1; 
-	if ($line =~ /\\lx (.*?)#((.*?)\\hm (\d*?)#)*/){
+write_to_log("Input file $ifsfm");
+#
+# opl the file - i.e. put each record on a line.
+#
+our @opld_file = opl_file($fpath.$ifsfm);
+#
+# Create a table of lexemes and syn in the following format
+#
+# Create hash of synonyms under lexeme[hm] as key
+#
+foreach my $line (@opld_file) 
+{
+	my $hm;
+	my $key;
+	my $sn = 1; 
+#	
+	if ($line =~ /\Q$lexmarker\E\s+(.*?)#((.*?)\Q$hommarker\E\s+(\d*?)#)*/)
+	{
 		$hm = $2 ? $4 : 0;
 		$key = $1;
 #
-		if ($hm != 0){
+		if ($hm != 0)
+		{
 			$key = $key.$hm;
 		}		
 #		
 		push @{$file_hash{$key}}, 0;
-#		
-		if ($line =~ /\\syn /)
+#
+		if ($line =~ /\Q$synmarker\E\s+/)
 		{
 			#retrieve sense and syn data
-			my @fields = split /#/, $line;	
-			foreach my $f (@fields){
-				if ($f =~ /\\sn (\d+)/){
+			our @fields = split /#/, $line;	
+			foreach my $f (@fields)
+			{
+				if ($f =~ /\Q$snsmarker\E\s+(\d+)/)
+				{
 					$sn = $1;
 				}
-				elsif ($f =~ /^\\syn\s+(.*)/){
+				elsif ($f =~ /^\Q$synmarker\E\s+(.*)/)
+				{
 					$f = $1." ".$sn;
 					push @{$file_hash{$key}}, $f;
 				}
@@ -121,25 +151,28 @@ our $pri_amb = "Y";
 our $pri_syn = "";
 our $pri_syn_sense = "";
 our $pri_syn_trunc = "";
-our @sec_lex_array = ();
-our $sec_lex = "";
-our $sec_syn = "";
-our @sec_syn_array = ();
+our @ref_lex_array = ();
+our $ref_lex = "";
+our $ref_syn = "";
+our @ref_syn_array = ();
 #
-our $recip_lex = "";
 our $ref_exists = "N";
-our $ref_reciprocated = "N";
-our $ref_ambiguous = "N";
+our $ref_reciprocated = "";
+our $ref_ambiguous = "";
 our $temp_lex = "";
+our $proposed_upd = "";
+our $temp_syn = "";
+our @recip_fnd_array = ();	
+our $recip_lex = "";
 #
 our $i = 0;
 #
 foreach $pri_lex (sort @lexemes) 
 {
 	$ref_exists = "N";
-	$ref_reciprocated = "N";
-	$ref_ambiguous = "N";
-	$recip_lex = "";
+	$ref_reciprocated = "";
+	$ref_ambiguous = "";
+	$ref_lex = "";
 #	
 	if ($pri_lex =~ /^(.*)\d$/g)
 	{
@@ -157,7 +190,7 @@ foreach $pri_lex (sort @lexemes)
 		for ($i=1; $i < scalar @{$file_hash{$pri_lex}}; $i++)
 		{
 # 
-			@sec_lex_array = ();
+			@ref_lex_array = ();
 			$pri_syn = @{$file_hash{$pri_lex}}[$i];	
 			$pri_syn =~ /^(.*)\s(\d+)/;	
 			$pri_syn_trunc = $1;
@@ -178,101 +211,135 @@ foreach $pri_lex (sort @lexemes)
 #
 				if ($temp_lex eq $pri_syn_trunc)		
 				{
-					push @sec_lex_array, $lexemes[$q]; 	
+					push @ref_lex_array, $lexemes[$q]; 	
 				}	
 			}
 #
 # No synonym referent - add line under primary 			
 #
-			if (scalar @sec_lex_array < 1)
+			if (scalar @ref_lex_array < 1)
 			{
 				$ref_exists = "N";
 				$ref_reciprocated = "N";
 				$ref_ambiguous = "N";
 				$recip_lex = "NONE";
+				$proposed_upd = "NONE";
 #				
 				if (@{$file_hash{$pri_lex}}[0] == 0)
 				{
 					$coll_id_ref = ++$coll_id_incr;
+					@{$file_hash{$pri_lex}}[0] = $coll_id_ref;
 				}
 				else
 				{
 					$coll_id_ref = @{$file_hash{$pri_lex}}[0];
 				}
-#				
-				upd_pri_ref();
+#		
+				push @{$coll_hash{$coll_id_ref}{$pri_lex}}, 
+					"###$pri_syn_sense###$pri_syn_trunc###$ref_exists###$recip_lex###$ref_ambiguous###";			
 			}
 			else
 			{
 #
 # Reference found
-# Single synonym referent - no ambiguity, check reciprocation
+# Check to see if reference is reciprocated
 #		
-				if (scalar @sec_lex_array == 1)
+				$ref_exists = "Y";
+				@recip_fnd_array = ();
+#	
+				for (my $t=0; $t < scalar @ref_lex_array; $t++)
 				{
-# Only one reference - not ambiguous
-					$ref_exists = "Y";
-					$ref_ambiguous = "N";	
-#				
-					is_reciprocal();
+					$ref_lex = $ref_lex_array[$t];
+#	
+					for (my $v=0; $v < scalar @{$file_hash{$ref_lex}}; $v++)
+					{
+						if (${$file_hash{$ref_lex}}[$v] =~ /^(.*?)\s+\d+$/)
+						{
+							$temp_syn = $1;
+						}
+						else 
+						{
+							$temp_syn = ${$file_hash{$ref_lex}}[$v];
+						}
+#			
+						if ($pri_lex_trunc eq $temp_syn)
+						{
+							push @recip_fnd_array, $ref_lex;
+						}
+					}
+				}
+#
+# Flag reciprocity
+#
+				if (scalar @recip_fnd_array == 0)
+				{
+					$ref_reciprocated = "N";
+					$recip_lex = "NONE";
 				}
 				else
 				{
-# Multiple synonym references, check reciprocity				
-					$ref_exists = "Y";
-					is_reciprocal();
-#
-					if (($recip_lex eq "") || ($recip_lex eq "AMBIG"))
-					{
-						$ref_ambiguous = "Y";
-					}
-					else
-					{
-						$ref_ambiguous = "N";
-					}
+					$ref_reciprocated = "Y";
+					($recip_lex) = @recip_fnd_array;
 				}
-# Was reference ambiguous?
+#
+# Flag ambiguity	
+#
+				if (scalar @ref_lex_array > 1 && $ref_reciprocated eq "N")	
+				{
+					$ref_ambiguous = "Y";
+				}
+				else
+				{
+					$ref_ambiguous = "N";
+				}
+#				
+# If reference is ambiguous, create unique collection, add to table
+#
 				if ($ref_ambiguous eq "Y")
 				{
 					$coll_id_ref = ++$coll_id_incr;
-					upd_pri_ref();
+					@{$file_hash{$pri_lex}}[0] = $coll_id_ref;
+					$proposed_upd = "NONE";
+#					
+					push @{$coll_hash{$coll_id_ref}{$pri_lex}}, 
+						"###$pri_syn_sense###$pri_syn_trunc###$ref_exists###$recip_lex###$ref_ambiguous###";	
 				}
+# Non-ambiguous reference, update collection IDs in primary & referent				
 				else
 				{
 					if (@{$file_hash{$pri_lex}}[0] > 0)
 					{
 						$coll_id_ref = @{$file_hash{$pri_lex}}[0];
-						upd_pri_ref();
-						upd_sec_ref();
+						@{$file_hash{$ref_lex}}[0] = $coll_id_ref;
 					}
-					elsif (@{$file_hash{$sec_lex}}[0] > 0)
+					elsif (@{$file_hash{$ref_lex}}[0] > 0)
 					{
-						$coll_id_ref = @{$file_hash{$sec_lex}}[0];		
-						upd_pri_ref();
+						$coll_id_ref = @{$file_hash{$ref_lex}}[0];		
+						@{$file_hash{$pri_lex}}[0] = $coll_id_ref;
 					}
 					else
 					{
 						$coll_id_ref = ++$coll_id_incr;
-						upd_pri_ref();
-						upd_sec_ref();
+						@{$file_hash{$pri_lex}}[0] = $coll_id_ref;		
+						@{$file_hash{$ref_lex}}[0] = $coll_id_ref;
 					}
+#					
+					push @{$coll_hash{$coll_id_ref}{$pri_lex}}, 
+						"###$pri_syn_sense###$pri_syn_trunc###$ref_exists###$recip_lex###$ref_ambiguous###";	
 				}
 			}
 		}
 	}
 }	
 #
-our $path = "C:\\Users\\public\\Documents\\common\\sil\\zaza\\ch_syn";
-#
-our $outfile = "$path\\syn_coll.txt";
 our $row = "";
 #
-open(my $fhoutfile, '>:encoding(UTF-8)', $outfile) 
-	or die "Could not open file '$outfile' $!";
+open(my $fhoutfile, '>:encoding(UTF-8)', $fpath.$ofrpt) 
+	or die "Could not open file '$ofrpt' $!";
 #
 # Print collections out as CSV
 #
-print $fhoutfile "Collection#Lexeme#Synonym#Ref Fnd#Reciprocal#Ambiguous\n";
+print $fhoutfile "Collection#Lexeme#Sense#Synonym#Ref Fnd#Reciprocal#Ambiguous#Proposed Update\n";
 # 
 # Sort by collection number, then lexeme
 #
@@ -282,8 +349,8 @@ foreach $coll_id_ref (sort {$a <=> $b} keys %coll_hash)
 	{
 		foreach $row (@{$coll_hash{$coll_id_ref}{$pri_lex}})
 		{
-			$row =~ /^###(.*)###(.*)###(.*)###(.*)###$/;
-			print $fhoutfile "$coll_id_ref#$pri_lex#$1#$2#$3#$4\n"; 
+			$row =~ /^###(.*)###(.*)###(.*)###(.*)###(.*)###$/;
+			print $fhoutfile "$coll_id_ref#$pri_lex#$1#$2#$3#$4#$5\n"; 
 		}	
 	}
 }
@@ -295,81 +362,13 @@ say "Hey";
 #print Dumper \%file_hash;
 #write_to_log ("Duplicate \\hm values have been found. SFM file must be corrected.");
 #print ("Duplicate homograph numbers found.  No data has been written. See details in log file.\n");
-
-#close $fhlogfile;
+#
+close $fhlogfile;
+#
 ######################  SUBROUTINES #################################
 
 sub write_to_log{
 
-        my ($message) = @_;
-	#print $fhlogfile "$message\n";
-}
-#
-sub is_reciprocal
-{
-	$recip_lex = "";
-	my $temp_syn = "";
-#	
-	for (my $t=0; $t < scalar @sec_lex_array; $t++)
-	{
-		$sec_lex = $sec_lex_array[$t];
-#	
-		for (my $v=0; $v < scalar @{$file_hash{$sec_lex}}; $v++)
-		{
-			if (${$file_hash{$sec_lex}}[$v] =~ /^(.*?)\s+\d+$/)
-			{
-				$temp_syn = $1;
-			}
-			else 
-			{
-				$temp_syn = ${$file_hash{$sec_lex}}[$v];
-			}
-#			
-			if ($pri_lex_trunc eq $temp_syn)
-			{
-				if ($recip_lex eq "")
-				{
-					$recip_lex = $sec_lex;
-				}
-				else
-				{
-					$recip_lex = "AMBIG";
-				}
-			}
-		}
-	}
-#
-	if (($recip_lex eq "") || ($recip_lex eq "AMBIG"))	
-	{}
-	else
-	{
-		$sec_lex = $recip_lex;
-	}
-#	
-	return;
-}				
-#
-sub	upd_pri_ref
-{
-# Update file_hash 
-	if ($recip_lex eq "") 
-	{
-		$recip_lex = "NONE";
-	}	
-#	
-	if ($ref_ambiguous eq "N")
-	{
-		@{$file_hash{$pri_lex}}[0] = $coll_id_ref;
-	}
-# Update collection_hash
-	push @{$coll_hash{$coll_id_ref}{$pri_lex}}, "###$pri_syn_trunc###$ref_exists###$recip_lex###$ref_ambiguous###";	
-#	
-	return;
-}
-#
-sub upd_sec_ref
-{
-	@{$file_hash{$sec_lex}}[0] = $coll_id_ref;
-#	push @{$coll_hash{$coll_id_ref}{$sec_lex}}, "###NOSYN###";	
-	return;
+    my ($message) = @_;
+	print $fhlogfile "$message\n";
 }
